6e3b8f5a95b68dd252afb429e2202dbe7d712866
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-12 18:22:23 +0900

    reconfigured app

--- index.js
+++ index.js
@@ -1,17 +1,6 @@
 // import React from "react"
 // import ReactDOM from "react-dom"
-import Header from "./Header"
-import MainContent from "./MainContent"
-import Footer from "./Footer"
+import App from "App"
 
-function App() {
-  return (
-    <div>
-      <Header />
-      <MainContent />
-      <Footer />
-    </div>
-  )
-}
 
 ReactDOM.render(<App />, document.getElementById("root"))
