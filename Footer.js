b59e545d5a4d564602b32724437db47bc72d1506
Steven Vasquez <s.vasquez85@gmail.com>
2022-04-12 09:56:41 +0900

    organizing components

---
+++ Footer.js
@@ -0,0 +1,7 @@
+export default function Footer() {
+  return (
+    <footer>
+      <small>© 2021 AngrySun development. All rights reserved.</small>
+    </footer>
+  )
+}
--- index.js
+++ index.js
@@ -1,36 +1,8 @@
 // import React from "react"
-
-function Header() {
-  return (
-    <header>
-      <nav>
-        {/* <img src="./react-logo.png" width="40px" /> */}
-      </nav>
-    </header>
-  )
-}
-
-function Footer() {
-  return (
-    <footer>
-      <small>© 2021 Ziroll development. All rights reserved.</small>
-    </footer>
-  )
-}
-
-function MainContent() {
-  return (
-    <div>
-      <h1>Reasons I'm excited to learn React</h1>
-      <ol>
-        <li>It's a popular library, so I'll be
-          able to fit in with the cool kids!</li>
-        <li>I'm more likely to get a job as a developer
-          if I know React</li>
-      </ol>
-    </div>
-  )
-}
+// import ReactDOM from "react-dom"
+import Header from "./Header"
+import MainContent from "./MainContent"
+import Footer from "./Footer"
 
 function Page() {
   return (
